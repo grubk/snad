a727ce1242db560cd4707dd1851ae343dd9b7797
kai <grubert.kai@gmail.com>
2026-08-15 12:52:33 -0700

    set datatype

---
+++ components/set.go
@@ -0,0 +1,20 @@
+package components
+
+type Set[T comparable] map[T]struct{}
+
+func CreateSet[T comparable]() Set[T] {
+	return make(Set[T])
+}
+
+func (s Set[T]) Add(value T) {
+	s[value] = struct{}{}
+}
+
+func (s Set[T]) Remove(value T) {
+	delete(s, value)
+}
+
+func (s Set[T]) Contains(value T) bool {
+	_, exists := s[value]
+	return exists
+}
